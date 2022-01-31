87f27da95f4cd1280772619befb06ce140c2ab05
mybabygrand <amuro_ray@kakao.com>
2022-01-31 12:10:07 +0900

    Section 3. Thymeleaf - Spring Integration And Form  - Single button1,2

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -12,7 +12,7 @@ public class Item {
     private Integer price;
     private Integer quantity;
 
-    private boolean salesOpen; //판매여부
+    private Boolean open; //판매여부
     private List<String> regions; //판매가능 지역
     private ItemType itemType; //상품 종류
     private String deliveryCode; //배송방식
